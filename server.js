385c04fe1499cd339929f189f4a48f8417b25023
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-05-24 04:45:33 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+const app = require("./app");
+
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+	console.log(`Hello World app is listening on port ${PORT} !`);
+});
+
+
